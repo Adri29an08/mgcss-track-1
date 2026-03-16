d51aee61af223b6d1f88232dee56501e7e0baf75
fernando <fernandocp2003@gmail.com>
2026-03-16 12:17:55 +0100

    feat: Arquitectura hexagonal base con entidad Solicitud en capa domain

---
+++ src/main/java/com/mgcss/domain/EstadoSolicitud.java
@@ -0,0 +1,8 @@
+// src/main/java/com/mgcss/track/domain/EstadoSolicitud.java
+package com.mgcss.domain;
+
+public enum EstadoSolicitud {
+    ABIERTA,
+    EN_PROCESO,
+    CERRADA
+}
--- src/test/java/com/mgcss/MgcssTrackApplicationTests.java
+++ src/test/java/com/mgcss/MgcssTrackApplicationTests.java
@@ -2,14 +2,12 @@ package com.mgcss;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class MgcssTrackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

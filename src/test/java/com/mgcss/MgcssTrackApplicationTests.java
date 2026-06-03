36269afadd8fc3c9092067855aa65d0678583439
Jaimeaq11 <jaimeabadq@gmail.com>
2026-06-03 22:48:33 +0200

    fix: segunda correcion del pipeline

--- src/test/java/com/mgcss/MgcssTrackApplicationTests.java
+++ src/test/java/com/mgcss/MgcssTrackApplicationTests.java
@@ -1,3 +1,5 @@
+package com.mgcss;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;

3d9e2168566e5910fe528e22b79405bb083246ee
Jaimeaq11 <jaimeabadq@gmail.com>
2026-06-03 18:35:53 +0200

    feat: implementacion adicional test-containers

--- src/test/java/com/mgcss/MgcssTrackApplicationTests.java
+++ src/test/java/com/mgcss/MgcssTrackApplicationTests.java
@@ -1,14 +1,33 @@
-package com.mgcss;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 @SpringBootTest
+@Testcontainers
 class MgcssTrackApplicationTests {
+
+
+    // Esto levanta un PostgreSQL real en Docker antes de los tests
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:15-alpine"
+    );
+
+    // Esto inyecta las credenciales temporales en Spring
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
-		
+        
     }
-
 }
